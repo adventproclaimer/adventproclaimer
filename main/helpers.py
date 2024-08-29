91daba26cbb6c8bb349165e7fba60b03e5de8429
lunyamwis <lutherlunyamwi@gmail.com>
2024-08-29 12:22:13 +0300

    testing

--- main/helpers.py
+++ main/helpers.py
@@ -128,7 +128,7 @@ def upload_file(file_content,file_name,file_content_type,id):
     """
     creds = get_creds('service_account_key.json','drive')
     temp_path = None
-    file_obj = ChunkedBytesIO(file_content.read())
+    file_obj = ChunkedBytesIO(file_content)
     
 
     
